bc09513f44a70bfa6c3755e1ab95cbc06a188acd
Jason Bao <bao.jason.z@gmail.com>
2025-05-14 16:28:31 -0400

    Add docker login (#6)

--- scripts/docker_push.sh
+++ scripts/docker_push.sh
@@ -7,6 +7,8 @@ while getopts "e:a:s:" opt; do
     esac
 done
 
+aws ecr get-login-password --region us-west-2 | docker login --username AWS --password-stdin ${aws_account_id}.dkr.ecr.us-west-2.amazonaws.com
+
 docker tag api ${aws_account_id}.dkr.ecr.us-west-2.amazonaws.com/${env}-api:latest
 docker tag api ${aws_account_id}.dkr.ecr.us-west-2.amazonaws.com/${env}-api:$(git rev-parse --short HEAD)
 docker push ${aws_account_id}.dkr.ecr.us-west-2.amazonaws.com/${env}-api:$(git rev-parse --short HEAD)
